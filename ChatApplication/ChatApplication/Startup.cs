e01d8558ef7f5e670c1e16a0258790807b73203c
radu-matei <radu.matei@equilobe.com>
2015-10-20 17:47:24 +0300

    Added OWIN Startup Class - mapped requests to the SignalR OWIN pipeline

---
+++ ChatApplication/ChatApplication/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ChatApplication.Startup))]
+
+namespace ChatApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
