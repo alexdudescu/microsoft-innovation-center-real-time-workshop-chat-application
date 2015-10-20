50b7103353bb870f40709bf152ad909f8584187f
radu-matei <radu.matei@equilobe.com>
2015-10-20 17:57:10 +0300

    Added ChatHub class - added server method for the clients to call

---
+++ ChatApplication/ChatApplication/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace ChatApplication
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string originatorUser, string message)
+        {
+            Clients.All.messageReceived(originatorUser, message);
+        }
+    }
+}
